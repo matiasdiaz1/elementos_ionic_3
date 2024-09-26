87d80c2e3f97f0e155965218733f4734d04bc9a6
Mati <mat.diaza@duocuc.cl>
2024-09-26 16:51:31 -0300

    se genera el codigo qr instalando qrcode falta hacer el diseño

--- src/app/pages/serviciopro/serviciopro.page.ts
+++ src/app/pages/serviciopro/serviciopro.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-serviciopro',
   templateUrl: './serviciopro.page.html',
   styleUrls: ['./serviciopro.page.scss'],
 })
-export class ServicioproPage implements OnInit {
+export class ServicioproPage {
+
+  qrCodeUrl: string = '';
 
   constructor() { }
 
-  ngOnInit() {
+  generateQRCode() {
+    const url = 'https://www.youtube.com/watch?v=f_WuRfuMXQw&ab_channel=SheeshBruh'; // URL del video de YouTube
+    QRCode.toDataURL(url, { errorCorrectionLevel: 'H' }, (err, qrCodeUrl) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      this.qrCodeUrl = qrCodeUrl;
+    });
   }
-
 }
+
