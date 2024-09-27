577259ac1aafe4d1e45eea615a0e2610cec4bb9a
Mati <mat.diaza@duocuc.cl>
2024-09-27 02:52:29 -0300

    escanear con camra qr estudiante

--- src/app/pages/servicioalum/servicioalum.page.ts
+++ src/app/pages/servicioalum/servicioalum.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-servicioalum',
@@ -9,7 +10,10 @@ import { MenuController } from '@ionic/angular';
 })
 export class ServicioalumPage {
 
-  constructor(private router: Router, private menu: MenuController) { }
+  constructor(
+    private router: Router,
+    private menu: MenuController
+  ) { }
 
   cerrarSesion() {
     localStorage.removeItem('usuario');
@@ -18,5 +22,33 @@ export class ServicioalumPage {
 
   openProfileMenu() {
     this.menu.open('profile-menu-alum');
+  }
+
+  async activarCamara() {
+    try {
+      
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        
+        BarcodeScanner.hideBackground();
+
+       
+        const result = await BarcodeScanner.startScan();
+
+
+        if (result.hasContent) {
+          console.log('Código QR escaneado:', result.content);
+
+        }
+      } else {
+        console.error('Permiso de cámara no concedido');
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error al escanear el código QR:', error.message);
+      } else {
+        console.error('Error desconocido:', error);
+      }
+    }
   }
 }
