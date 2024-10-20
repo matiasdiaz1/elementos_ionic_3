e136546ab44982b711a117a587732fa14faac903
Mati <mat.diaza@duocuc.cl>
2024-10-20 16:14:38 -0300

    se agrego almacenamiento

---
+++ src/app/services/almacenamiento.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocaldbService {
+
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  public async get(key: string) {
+    const valores= await this._storage?.get(key);
+    return valores;
+  }
+
+  public async remover(key:string){
+    await this._storage?.remove(key);
+  }
+
+  }
